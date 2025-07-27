4d19fb421b97e02a6aa8a8b61f41c22ac03ae8fd
Harish Chouhan <me@harishchouhan.com>
2025-07-27 23:37:13 +0700

    Fix deprecated false-to-array conversions, remove WP_PLUGIN_URL usage, and clean up outdated references

--- woocommerce-branding.php
+++ woocommerce-branding.php
@@ -14,61 +14,38 @@
  * Text Domain:       woocommerce-branding
  *
  * @package Themeist_WooCommerceBranding
-
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
+
 if ( ! class_exists( 'DOT_WooCommerce_Branding' ) ) {
 
-
 	class DOT_WooCommerce_Branding {
-
-		/*
-		--------------------------------------------*
-		 * Constructor
-		 *--------------------------------------------*/
 
 		/**
 		 * Initializes the plugin by setting localization, filters, and administration functions.
 		 */
 		function __construct() {
-
-			// Load text domain
 			add_action( 'init', array( $this, 'load_localisation' ), 0 );
+			add_action( 'admin_menu', array( $this, 'dot_wcb_menu' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'dot_wcb_assets' ) );
+			add_action( 'admin_init', array( $this, 'dot_wcb_settings' ) );
 
-			// Adding Plugin Menu
-			add_action( 'admin_menu', array( &$this, 'dot_wcb_menu' ) );
-
-			// Load our custom assets.
-			add_action( 'admin_enqueue_scripts', array( &$this, 'dot_wcb_assets' ) );
-
-			// Register Settings
-			add_action( 'admin_init', array( &$this, 'dot_wcb_settings' ) );
-
-			// WooCommerce Branding
-			/**
-			 * Check if WooCommerce is active
-			 */
 			if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-				add_filter( 'gettext', array( &$this, 'dot_wcb_woocommerce_menu_title' ) );
-				add_filter( 'ngettext', array( &$this, 'dot_wcb_woocommerce_menu_title' ) );
-
-				// Add WooCommerce Icon to website backend
-				add_action( 'admin_head', array( &$this, 'dot_wcb_woocommerce_icon' ) );
+				add_filter( 'gettext', array( $this, 'dot_wcb_woocommerce_menu_title' ) );
+				add_filter( 'ngettext', array( $this, 'dot_wcb_woocommerce_menu_title' ) );
+				add_action( 'admin_head', array( $this, 'dot_wcb_woocommerce_icon' ) );
 			}
-		} // end constructor
+		}
 
-
-		/*
-		--------------------------------------------*
-		 * Localisation | Public | 1.0 | Return : void
-		 *--------------------------------------------*/
-
+		/**
+		 * Load plugin localisation.
+		 */
 		public function load_localisation() {
 			load_plugin_textdomain( 'dot_wcb', false, basename( __DIR__ ) . '/languages' );
-		} // End load_localisation()
+		}
 
 		/**
 		 * Defines constants for the plugin.
@@ -77,169 +54,192 @@ if ( ! class_exists( 'DOT_WooCommerce_Branding' ) ) {
 			define( 'DOT_WCB_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 		}
 
-		/*
-		--------------------------------------------*
-		 * Admin Menu
-		 *--------------------------------------------*/
-
+		/**
+		 * Register the admin settings page.
+		 */
 		function dot_wcb_menu() {
 			$page_title = __( 'WC Branding', 'dot_wpfmc' );
 			$menu_title = __( 'WC Branding', 'dot_wpfmc' );
 			$capability = 'manage_options';
 			$menu_slug  = 'dot_wcb';
-			$function   = array( &$this, 'dot_wcb_menu_contents' );
+			$function   = array( $this, 'dot_wcb_menu_contents' );
 			add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function );
-		}   //dot_wpfmc_menu
+		}
 
-		/*
-		--------------------------------------------*
-		 * Load Necessary JavaScript Files
-		 *--------------------------------------------*/
-
+		/**
+		 * Load admin assets on settings page.
+		 */
 		function dot_wcb_assets() {
-			if ( isset( $_GET['page'] ) && $_GET['page'] == 'dot_wcb' ) {
-
-				wp_enqueue_style( 'thickbox' ); // Stylesheet used by Thickbox
+			if ( isset( $_GET['page'] ) && $_GET['page'] === 'dot_wcb' ) {
+				wp_enqueue_style( 'thickbox' );
 				wp_enqueue_script( 'thickbox' );
 				wp_enqueue_script( 'media-upload' );
 
-				wp_register_script( 'dot_wcb_admin', WP_PLUGIN_URL . '/woocommerce-branding/js/dot_wcb_admin.js', array( 'thickbox', 'media-upload' ) );
+				wp_register_script(
+					'dot_wcb_admin',
+					plugins_url( 'js/dot_wcb_admin.js', __FILE__ ),
+					array( 'thickbox', 'media-upload' ),
+					null,
+					true
+				);
 				wp_enqueue_script( 'dot_wcb_admin' );
 			}
-		} //dot_wpfmc_assets
+		}
 
-		/*
-		--------------------------------------------*
-		 * Settings & Settings Page
-		 *--------------------------------------------*/
-
+		/**
+		 * Register plugin settings and fields.
+		 */
 		public function dot_wcb_settings() {
+			register_setting( 'dot_wcb_settings', 'dot_wcb_settings', array( $this, 'settings_validate' ) );
 
-			// Settings
-			register_setting( 'dot_wcb_settings', 'dot_wcb_settings', array( &$this, 'settings_validate' ) );
-
-			/**
-			 * Check if WooCommerce is active
-			 */
 			if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+				add_settings_section(
+					'woocommerce_branding',
+					__( 'Replace WC Branding', 'dot_wcb' ),
+					array( $this, 'section_woocommerce_branding' ),
+					'dot_wcb_settings'
+				);
 
-				// WooCommerce Branding
-				add_settings_section( 'woocommerce_branding', __( 'Replace WC Branding', 'dot_wcb' ), array( &$this, 'section_woocommerce_branding' ), 'dot_wcb_settings' );
+				add_settings_field(
+					'woocommerce_branding_name',
+					__( 'Name', 'dot_wcb' ),
+					array( $this, 'section_woocommerce_branding_name' ),
+					'dot_wcb_settings',
+					'woocommerce_branding'
+				);
 
-				add_settings_field( 'woocommerce_branding_name', __( 'Name', 'dot_wcb' ), array( &$this, 'section_woocommerce_branding_name' ), 'dot_wcb_settings', 'woocommerce_branding' );
-
-				add_settings_field( 'woocommerce_branding_icon', __( 'Icon URL', 'dot_wcb' ), array( &$this, 'section_woocommerce_branding_icon' ), 'dot_wcb_settings', 'woocommerce_branding' );
-
+				add_settings_field(
+					'woocommerce_branding_icon',
+					__( 'Icon URL', 'dot_wcb' ),
+					array( $this, 'section_woocommerce_branding_icon' ),
+					'dot_wcb_settings',
+					'woocommerce_branding'
+				);
 			}
-		}   //dot_wcb_settings
+		}
 
-
-		/*
-		--------------------------------------------*
-		 * Settings & Settings Page
-		 * dot_wpfmc_menu_contents
-		 *--------------------------------------------*/
-
+		/**
+		 * Output the settings page contents.
+		 */
 		public function dot_wcb_menu_contents() {
 			?>
 			<div class="wrap">
-				<!--<div id="icon-freshdesk-32" class="icon32"><br></div>-->
 				<div id="icon-options-general" class="icon32"><br></div>
 				<h2><?php _e( 'WC Branding', 'dot_wcb' ); ?></h2>
 
 				<form method="post" action="options.php">
-					<?php // wp_nonce_field('update-options'); ?>
-					<?php settings_fields( 'dot_wcb_settings' ); ?>
-					<?php do_settings_sections( 'dot_wcb_settings' ); ?>
+					<?php
+					settings_fields( 'dot_wcb_settings' );
+					do_settings_sections( 'dot_wcb_settings' );
+					?>
 					<p class="submit">
 						<input name="Submit" type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'dot_wcb' ); ?>" />
 					</p>
 				</form>
 			</div>
-
 			<?php
-		}   //dot_wpfmc_menu_contents
-
-		function section_woocommerce_branding() {
-
-			// _e( 'Replace WooComm. branding with your own', 'dot_wcb' );
 		}
 
+		/**
+		 * Outputs the section description (currently empty).
+		 */
+		function section_woocommerce_branding() {
+			// Intentionally left blank.
+		}
+
+		/**
+		 * Outputs the input field for the WooCommerce branding name setting.
+		 */
 		function section_woocommerce_branding_name() {
 			$options = get_option( 'dot_wcb_settings' );
-
+			if ( ! is_array( $options ) ) {
+				$options = array();
+			}
+			$branding_name = sanitize_text_field( $options['woocommerce_branding_name'] ?? '' );
 			?>
-				<input type='text' id='dot_wcb_settings[woocommerce_branding_name]' class='regular-text' name='dot_wcb_settings[woocommerce_branding_name]' value='<?php echo sanitize_text_field( $options['woocommerce_branding_name'] ); ?>'/>
+			<input
+				type="text"
+				id="dot_wcb_settings[woocommerce_branding_name]"
+				name="dot_wcb_settings[woocommerce_branding_name]"
+				class="regular-text"
+				value="<?php echo esc_attr( $branding_name ); ?>"
+			/>
 			<?php
 		}
 
+		/**
+		 * Outputs the input field for the WooCommerce branding icon URL setting.
+		 */
 		function section_woocommerce_branding_icon() {
 			$options = get_option( 'dot_wcb_settings' );
+			if ( ! is_array( $options ) ) {
+				$options = array();
+			}
+			$icon_url = esc_url( $options['woocommerce_branding_icon'] ?? '' );
 			?>
-			<span class='upload'>
-				<input type='text' id='dot_wcb_settings[woocommerce_branding_icon]' class='regular-text text-upload' name='dot_wcb_settings[woocommerce_branding_icon]' value='<?php echo esc_url( $options['woocommerce_branding_icon'] ); ?>'/>
-				<input type='button' class='button button-upload' value='Upload an Icon'/></br>
-				<img style='max-width: 300px; display: block;' src='<?php echo esc_url( $options['woocommerce_branding_icon'] ); ?>' class='preview-upload' /><br>
+			<span class="upload">
+				<input
+					type="text"
+					id="dot_wcb_settings[woocommerce_branding_icon]"
+					class="regular-text text-upload"
+					name="dot_wcb_settings[woocommerce_branding_icon]"
+					value="<?php echo $icon_url; ?>"
+				/>
+				<input type="button" class="button button-upload" value="<?php esc_attr_e( 'Upload an Icon', 'dot_wcb' ); ?>" /><br>
+				<img style="max-width: 300px; display: block;" src="<?php echo $icon_url; ?>" class="preview-upload" /><br>
 				<?php _e( 'Icon should be 28px x 28px', 'dot_wcb' ); ?>
-
 			</span>
 			<?php
 		}
 
-		/*
-		--------------------------------------------*
-		 * Settings Validation
-		 *--------------------------------------------*/
-
+		/**
+		 * Validate and sanitize plugin settings before saving.
+		 *
+		 * @param array $input Settings array.
+		 * @return array Sanitized settings.
+		 */
 		function settings_validate( $input ) {
-
 			return $input;
 		}
 
-		/*
-		--------------------------------------------*
-		 * Plugin Functions
-		 *--------------------------------------------*/
-
-
+		/**
+		 * Replaces the WooCommerce menu label with the custom branding name.
+		 *
+		 * @param string $translated The translated string.
+		 * @return string Modified menu title if branding name is set, original otherwise.
+		 */
 		function dot_wcb_woocommerce_menu_title( $translated ) {
 			$options = get_option( 'dot_wcb_settings' );
-			if ( ! isset( $options['woocommerce_branding_name'] ) ) {
-				$options['woocommerce_branding_name'] = '';
+			if ( ! is_array( $options ) ) {
+				$options = array();
 			}
-
-			if ( $options['woocommerce_branding_name'] == '' ) {
-				return $translated;
-
-			} else {
-				$translated = str_replace( 'WooCommerce', sanitize_text_field( $options['woocommerce_branding_name'] ), $translated );
-				$translated = str_replace( 'WooCommerce', sanitize_text_field( $options['woocommerce_branding_name'] ), $translated );
-				return $translated;
-			}
+			$branding_name = sanitize_text_field( $options['woocommerce_branding_name'] ?? '' );
+			return $branding_name !== '' ? str_replace( 'WooCommerce', $branding_name, $translated ) : $translated;
 		}
 
+		/**
+		 * Injects custom CSS to replace the WooCommerce admin menu icon with a custom branding icon.
+		 */
 		function dot_wcb_woocommerce_icon() {
-
 			$options = get_option( 'dot_wcb_settings' );
-
-			if ( $options['woocommerce_branding_icon'] != '' ) {
-				echo '<style type="text/css">
+			if ( ! is_array( $options ) ) {
+				return;
+			}
+			$icon_url = esc_url( $options['woocommerce_branding_icon'] ?? '' );
+			if ( $icon_url !== '' ) {
+				?>
+				<style type="text/css">
 					#adminmenu #toplevel_page_woocommerce div.wp-menu-image {
-						background-image: url(' . esc_url( $options['woocommerce_branding_icon'] ) . ') ;
+						background-image: url('<?php echo $icon_url; ?>');
 						background-size: auto;
 						background-position: 0 0;
 					}
-	        		</style>';
+				</style>
+				<?php
 			}
 		}
-	} // End Class
-
+	} // End class
 
 	// Initiation call of plugin
 	$dot_woocommerce_branding = new DOT_WooCommerce_Branding( __FILE__ );
-
 }
-
-
-
-?>
